e9efc63a721dfabe03a06365cf329f5b23b76788
anhzudeptrai2 <86923443+anhzudeptrai2@users.noreply.github.com>
2024-04-19 18:46:07 +0700

    Update main.cpp
    update nut bam + tactics 19/4/2024 by Vuz

--- src/main.cpp
+++ src/main.cpp
@@ -1,304 +1,329 @@
 #include <Arduino.h>
-int RPWM1 = 7; // động cơ trái
+int RPWM1 = 7;
 int LPWM1 = 6;
-int RPWM = 8; // động cơ phải
+int RPWM = 8;
 int LPWM = 9;
-
-int IRFL = 2;  // IR trước trai
-int IRFR = 3;  // IR truoc phai
-int IRCB = 21; // IR sau
-
+int IRR = 2;
+int IRL = 3;
+int IRB = 22;
 int trig1 = 4; // SR trướcttttt
 int echo1 = 5;
 int trig2 = 12; // SR phải
 int echo2 = 13;
 int trig3 = 10; // SR trái
 int echo3 = 11;
-
-int Button = 22;
-int valButton;
-
-int valIRFL;
-int valIRFR;
-int valIRCB;
-
+int valIRR;
+int valIRL;
+int valIRB;
+#define button_1 A0 // reset xe ngung hoat dong
+#define button_2 A1 // delay 3s chay default
+#define button_3 A2 // tactics ...
+unsigned long previousSR_millis = 0;
 unsigned long duration1, duration2, duration3;
-
+int scan = 0;
 int distance1;
 int distance2;
 int distance3;
+int lastdistance1 = 0;
+int lastdistance2 = 0;
+int lastdistance3 = 0;
 
 void FORWARD()
 {
-  Serial.println("Tien");
-  analogWrite(LPWM, 155);
-  analogWrite(LPWM1, 155);
+  analogWrite(LPWM, 255);
+  analogWrite(LPWM1, 255);
   analogWrite(RPWM, 0);
   analogWrite(RPWM1, 0);
 }
-
 void BACKWARD()
 {
-  Serial.println("Lui");
   analogWrite(LPWM, 0);
   analogWrite(LPWM1, 0);
   analogWrite(RPWM, 155);
   analogWrite(RPWM1, 155);
 }
 
-void Stop()
+void STOP()
 {
-  Serial.println("Dung");
   analogWrite(LPWM, 0);
   analogWrite(RPWM, 0);
   analogWrite(LPWM1, 0);
   analogWrite(RPWM1, 0);
+}
+void THOATHIEM()
+{
+  analogWrite(LPWM, 0);
+  analogWrite(RPWM, 255);
+  analogWrite(LPWM1, 0);
+  analogWrite(RPWM1, 255);
 }
 
-void Rotate()
+void ROTATER()
 {
-  Serial.println("Rotate"); // xoay tại chỗ
-  analogWrite(LPWM, 100);
+  analogWrite(LPWM, 125);
   analogWrite(RPWM, 0);
   analogWrite(LPWM1, 0);
-  analogWrite(RPWM1, 100);
+  analogWrite(RPWM1, 125);
+  delay(500);
 }
-void RightBackWard()
+void ROTATEL()
 {
-  Serial.println("lui phai");
-  analogWrite(RPWM, 0);
-  analogWrite(LPWM, 100);
+  analogWrite(LPWM, 0);
+  analogWrite(RPWM, 125);
+  analogWrite(LPWM1, 125);
   analogWrite(RPWM1, 0);
-  analogWrite(LPWM1, 50);
+  delay(500);
 }
-void LeftBackWard()
-{
-  Serial.println("lui trai");
-  analogWrite(RPWM, 0);
-  analogWrite(RPWM1, 0);
-  analogWrite(LPWM, 50);
-  analogWrite(LPWM1, 100);
-}
-void RightForWard()
-{
-  Serial.println("tien phai");
-  analogWrite(RPWM, 100);
-  analogWrite(RPWM1, 50);
-  analogWrite(LPWM, 0);
-  analogWrite(LPWM1, 0);
-}
-void LeftForWard()
-{
-  Serial.println("tien trai");
-  analogWrite(RPWM, 50);
-  analogWrite(RPWM1, 100);
-  analogWrite(LPWM, 0);
-  analogWrite(LPWM, 0);
-}
-void Push()
+// void RBACKWARD()
+// {
+//   analogWrite(RPWM, 0);
+//   analogWrite(LPWM, 100);
+//   analogWrite(RPWM1, 0);
+//   analogWrite(LPWM1, 50);
+// }
+// void LBACKWARD()
+// {
+//   analogWrite(RPWM, 0);
+//   analogWrite(RPWM1, 0);
+//   analogWrite(LPWM, 50);
+//   analogWrite(LPWM1, 100);
+// }
+// void RFORWARD()
+// {
+//   analogWrite(RPWM, 20);
+//   analogWrite(RPWM1, 0);
+//   analogWrite(LPWM, 120);
+//   analogWrite(LPWM1, 0);
+// }
+// void LFORWARD()
+// {
+//   analogWrite(RPWM, 0);
+//   analogWrite(RPWM1, 20);
+//   analogWrite(LPWM, 0);
+//   analogWrite(LPWM1, 120);
+// }
+void PUSH()
 {
   analogWrite(LPWM, 255);
   analogWrite(RPWM, 0);
   analogWrite(LPWM1, 255);
   analogWrite(RPWM1, 0);
 }
-void search()
-{
-   int scan = 0;
-   if (distance1 <= 25) // SR trước
-  {
-    Serial.println(" dam bang dau");
-    Push();
-  }
-  else if (distance2 <= 25) // SR Trái
-  {
-    Serial.println("re trai r dam");
-    LeftForWard();
-    Push();
-  }
-  else if (distance3 <= 25) // SR Phai
-  {
-    Serial.println("re phai roi dam");
-    RightForWard();
-    Push();
-  }
-
-  else
-  {
-    if (scan < 3)
-    {
-      analogWrite(RPWM, 80);
-      analogWrite(LPWM, 0);
-      analogWrite(RPWM1, 0);
-      analogWrite(LPWM1, 50);
-    }
-    else if (scan <= 5)
-    {
-      analogWrite(RPWM, 0);
-      analogWrite(LPWM, 80);
-      analogWrite(RPWM1, 50);
-      analogWrite(LPWM1, 0);
-    }
-    if (scan < 5)
-    {
-      scan += 1;
-    }
-    else if (scan == 5)
-    {
-      scan = 0;
-    }
-  }
-}
-
-boolean caseInterrupt0 = false;
-boolean caseInterrupt1 = false;
-boolean caseInterrupt2 = false;
-
-long timechange = 0;
 
 void proval()
 {
-  unsigned time = 500;
-  while (caseInterrupt0 && millis() - timechange < time)
-    ;
-  while (caseInterrupt1 && millis() - timechange < time)
-    ;
-  while (caseInterrupt2 && millis() - timechange < time)
-    ;
-  // khai bao ham valIR
-  valIRFR = digitalRead(IRFR);
-  valIRFL = digitalRead(IRFL);
-  valIRCB = digitalRead(IRCB);
+  valIRR = digitalRead(IRR);
+  valIRL = digitalRead(IRL);
+  valIRB = digitalRead(IRB);
 
-  caseInterrupt0 = false; // reset TH ngat
-  caseInterrupt1 = false;
-  caseInterrupt2 = false;
-}
-void Interrupt0() // IRFR == 0
-{
-  Stop();
-  delay(50);
-  LeftBackWard();
-  search();
-  caseInterrupt0 = true;
-  timechange = millis();
-
-  caseInterrupt1 = false;
-  caseInterrupt2 = false;
-}
-void Interrupt1() // IRFL == 0
-{
-  Stop();
-  delay(50);
-  RightBackWard();
-  search();
-  caseInterrupt1 = true;
-  timechange = millis();
-  caseInterrupt0 = false;
-  caseInterrupt2 = false;
-}
-void Interrupt2() // IRCB == 0
-{
-  Stop();
-  delay(50);
-  FORWARD();
-  search();
-  caseInterrupt2 = true;
-  timechange = millis();
-  caseInterrupt0 = false;
-  caseInterrupt1 = false;
-}
-void setup()
-{
-  Serial.begin(9600);
-  delay(1000);
-  // dong co
-  pinMode(RPWM, OUTPUT);
-  pinMode(LPWM, OUTPUT);
-  pinMode(RPWM1, OUTPUT);
-  pinMode(LPWM1, OUTPUT);
-  // IR
-  pinMode(IRFL, INPUT);
-  pinMode(IRFR, INPUT);
-  pinMode(IRCB, INPUT);
-  // SR
-  pinMode(trig1, OUTPUT);
-  pinMode(trig2, OUTPUT);
-  pinMode(echo1, INPUT);
-  pinMode(echo2, INPUT);
-  pinMode(trig3, OUTPUT);
-  pinMode(echo3, INPUT);
-  // chay ham ngat
-  attachInterrupt(0, Interrupt0, RISING);
-  attachInterrupt(1, Interrupt1, RISING);
-  attachInterrupt(2, Interrupt2, RISING);
-}
-
-void loop()
-{
-  // IR truoc phai
-  valIRFR = digitalRead(IRFR);
-  Serial.print("IR truoc phai: ");
-  Serial.println(valIRFR);
-  // IR truoc trai
-  valIRFL = digitalRead(IRFL);
-  Serial.print("IR truoc trai: ");
-  Serial.println(valIRFL);
-  // IR sau
-  valIRCB = digitalRead(IRCB);
-  Serial.print("IR trung tam: ");
-  Serial.println(valIRCB);
-  // SR04 truoc
-  digitalWrite(trig1, 0);
-  delayMicroseconds(2);
-  digitalWrite(trig1, 1);
-  delayMicroseconds(5);
-  digitalWrite(trig1, 0);
-  duration1 = pulseIn(echo1, HIGH);
-  distance1 = int(duration1 / 2 / 29.412);
-  // SR phai
-  digitalWrite(trig2, 0);
-  delayMicroseconds(2);
-  digitalWrite(trig2, 1);
-  delayMicroseconds(5);
-  digitalWrite(trig2, 0);
-  duration2 = pulseIn(echo2, HIGH);
-  distance2 = int(duration2 / 2 / 29.412);
-  // SR trai
-  digitalWrite(trig3, 0);
-  delayMicroseconds(2);
-  digitalWrite(trig3, 1);
-  delayMicroseconds(5);
-  digitalWrite(trig3, 0);
-  duration3 = pulseIn(echo3, HIGH);
-  distance3 = int(duration3 / 2 / 29.412);
-
-  if (distance1 >= 1 && distance1 <= 25)
+  if (valIRR == 0)
   {
-    Serial.print(distance1);
-    Serial.println("cm");
+    // delay(100);
+    BACKWARD();
+  }
+  else if (valIRR == 0)
+  {
+    // delay(100);
+    BACKWARD();
+  }
+  else if (valIRL == 0 && valIRR == 0)
+  {
+    THOATHIEM();
+  }
+  else if (valIRB == 0)
+  {
+    // delay(100);
+    FORWARD();
+  }
+}
+int filterValueF()
+{
+  if (abs(distance1 - lastdistance1) > 50)
+  {
+    lastdistance1 = distance1;
+    return min(lastdistance1, 50);
   }
   else
   {
-    Serial.println("value cannot be read");
+    return min(distance1, 50);
   }
-  if (distance2 >= 1 && distance2 <= 15)
-  {
-    Serial.print(distance2);
-    Serial.println("cm");
-  }
-  else
-  {
-    Serial.println("value cannot be read");
-  }
-  if (distance3 >= 1 && distance3 <= 15)
-  {
-    Serial.print(distance3);
-    Serial.println("cm");
-  }
-  else
-  {
-    Serial.println("value cannot be read");
-  }
-  delay(1000);
 }
+int filterValR()
+{
+  if (abs(distance2 - lastdistance2) > 25)
+  {
+    lastdistance2 = distance2;
+    return min(lastdistance2, 50);
+  }
+  else
+  {
+    return min(distance2, 50);
+  }
+}
+int filterValL()
+{
+  if (abs(distance3 - lastdistance3) > 25)
+  {
+    lastdistance3 = distance3;
+    return min(lastdistance3, 50);
+  }
+  else
+  {
+    return min(distance3, 50);
+  }
+}
+
+void search()
+{
+  // unsigned long currnentSR_millis = millis();
+  // if (currnentSR_millis - previousSR_millis > 200)
+  // {
+  //   previousSR_millis = currnentSR_millis;
+  if (filterValueF() <= 40)
+  {
+    Serial.println("pushF");
+    PUSH();
+  }
+  else if (filterValR() <= 20)
+  {
+    Serial.println("pushR");
+    ROTATER();
+  }
+  else if (filterValL() <= 20)
+  {
+    Serial.println("pushL");
+    ROTATEL();
+  }
+  else
+  {
+    // if (scan < 3)
+    // {
+    //   ROTATER();
+    // }
+    // else if (scan <= 5)
+    // {
+    //   ROTATER();
+    // }
+    // if (scan < 5)
+    // {
+    //   scan += 1;
+    // }
+    // else if (scan == 5)
+    // {
+    //   scan = 0;
+    // }
+    analogWrite(LPWM, 120);
+    analogWrite(RPWM, 0);
+    analogWrite(LPWM1, 0);
+    analogWrite(RPWM1, 120);
+  }
+  // }
+}
+void tactic1()
+{
+  unsigned long currnentSR_millis = millis();
+  if (currnentSR_millis - previousSR_millis > 2000)
+  {
+    previousSR_millis = currnentSR_millis;
+    analogWrite(LPWM, 255);
+    analogWrite(RPWM, 0);
+    analogWrite(LPWM1, 155);
+    analogWrite(RPWM1, 0);
+    search();
+  }
+}
+
+  void setup()
+  {
+    Serial.begin(9600);
+    // DC
+    pinMode(RPWM, OUTPUT);
+    pinMode(LPWM, OUTPUT);
+    pinMode(RPWM1, OUTPUT);
+    pinMode(LPWM1, OUTPUT);
+    // IR
+    pinMode(IRR, INPUT);
+    pinMode(IRL, INPUT);
+    pinMode(IRB, INPUT);
+    // SR
+    pinMode(trig1, OUTPUT);
+    pinMode(trig2, OUTPUT);
+    pinMode(trig3, OUTPUT);
+    pinMode(echo1, INPUT);
+    pinMode(echo2, INPUT);
+    pinMode(echo3, INPUT);
+    // nut bam
+    pinMode(button_1, INPUT_PULLUP);
+    pinMode(button_2, INPUT_PULLUP);
+    pinMode(button_3, INPUT_PULLUP);
+    // ngat
+    attachInterrupt(0, proval, CHANGE);
+    attachInterrupt(1, proval, CHANGE);
+    attachInterrupt(3, proval, CHANGE);
+  }
+
+  void loop()
+  {
+    if (digitalRead(button_1) == LOW)
+    {
+      // trang thai ngat 
+    }
+    else if (digitalRead(button_2) == LOW)
+    {
+      // tieu chuan 
+      delay(2900);
+      search();
+    }
+    else if (digitalRead(button_3) == LOW)
+    {
+      // chien thuat chay mu 2s sau do bat cam bien 
+      delay(2800);
+      tactic1();
+    }
+  
+
+    // IR
+    valIRR = digitalRead(IRR);
+    Serial.print("IR phai: ");
+    Serial.println(valIRL);
+    valIRL = digitalRead(IRL);
+    Serial.print("IR trai: ");
+    Serial.println(valIRL);
+    valIRB = digitalRead(IRB);
+    Serial.print("IR sau: ");
+    Serial.println(valIRB);
+
+    // SR04 truoc
+    digitalWrite(trig1, 0);
+    delayMicroseconds(2);
+    digitalWrite(trig1, 1);
+    delayMicroseconds(5);
+    digitalWrite(trig1, 0);
+    duration1 = pulseIn(echo1, HIGH);
+    distance1 = int(duration1 / 2 / 29.412);
+    // SR04 phai
+    digitalWrite(trig2, 0);
+    delayMicroseconds(2);
+    digitalWrite(trig2, 1);
+    delayMicroseconds(5);
+    digitalWrite(trig2, 0);
+    duration2 = pulseIn(echo2, HIGH);
+    distance2 = int(duration2 / 2 / 29.412);
+    // SR04 trai
+    digitalWrite(trig3, 0);
+    delayMicroseconds(2);
+    digitalWrite(trig3, 1);
+    delayMicroseconds(5);
+    digitalWrite(trig3, 0);
+    duration3 = pulseIn(echo3, HIGH);
+    distance3 = int(duration3 / 2 / 29.412);
+    filterValL();
+    filterValR();
+    filterValueF();
+    // Serial.print(distance1);
+    // Serial.println("cm");
+    // Serial.print(distance2);
+    // Serial.println("cm");
+    // Serial.print(distance3);
+    // Serial.println("cm");
+  }
